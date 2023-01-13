8ba06856b0950b04e3dbe4fb529beb12dc03ed5e
janani-ob <jananiob@gmail.com>
2023-01-13 09:36:29 +0530

    fetching of files has been done

--- src/app/player-list/player-list.component.ts
+++ src/app/player-list/player-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import playerDetails from '../player-list.json';
+
+interface Player {
+  id: Number;
+  name: String;
+  age: Number;
+  gender: String;
+  status: String;
+}
 
 @Component({
   selector: 'app-player-list',
   templateUrl: './player-list.component.html',
   styleUrls: ['./player-list.component.css']
 })
-export class PlayerListComponent {
+export class PlayerListComponent implements OnInit {
+  constructor() { }
+
+  players: Player[] = playerDetails;
+
+  ngOnInit(): void { }
+
 
 }

1510ce894d638130f88c9b1d13ba1477e9c8c82d
Janani Boopathi <jananiob@gmail.com>
2023-03-31 22:14:55 +0530

    changes done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -18,7 +19,7 @@ import { RestApiComponent } from './rest-api/rest-api.component';
 @NgModule({
   declarations: [
     AppComponent,
-    RestApiComponent
+    RestApiComponent,
   ],
   imports: [
     BrowserModule,
